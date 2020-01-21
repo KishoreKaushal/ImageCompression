8583db2bef033f5de7837b9b28f67e9ae10aa6b8
Kaushal Kishore <kshr4kshl@gmail.com>
2020-01-21 20:45:42 +0530

    require number of principle component

--- PCA/main.py
+++ PCA/main.py
@@ -1,12 +1,28 @@
 import argparse
-import numpy as np 
-from matplotlib.image import imread
+import numpy as np
+import cv2
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA, IncrementalPCA
 
-def required_components(variance=90.0):
-    return 0
+def required_num_components(single_channel_img, variance_percentage=90.0):
+    img = np.array(single_channel_img, dtype=np.float)
+    assert img.ndim == 2, ("2D tensors are expected")
+    
+    pca = PCA()
+    pca.fit(img)
+    cumulative_variance = np.cumsum(pca.explained_variance_ratio_) * 100
 
+    # How many PC's can represent {variance} % of image?
+    num_components = np.argmax(cumulative_variance > variance_percentage)
+    return num_components
+    
+
+def pca_compress(img, k):
+    pass
+
+
+def pca_decompress(data):
+    pass
 
 def main():
     parser = argparse.ArgumentParser(description='Using the PCA for image compression.')
@@ -21,7 +37,20 @@ def main():
     print("Output Image: {}".format(args.output))
     print("Total Variance: {}".format(args.var))
 
-    k = required_components(args.var)
+    img = cv2.imread(args.input)
+
+    separate_channels = []
+
+    # separating RGB channels
+    if img.ndim == 3:
+        for i in range(img.shape[2]):
+            separate_channels.append(img[:,:,i])
+    else:
+        separate_channels.append(img)
+
+    num_components = list(map(required_num_components, separate_channels, 
+                        np.ones(len(separate_channels), dtype=np.float) * args.var))
+
 
 
 if __name__=="__main__":
